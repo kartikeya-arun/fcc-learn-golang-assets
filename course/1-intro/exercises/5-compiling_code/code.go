8a82acc440ea1a78e22609a64de476b2d01aada3
kartikeya-arun <kartikeya7.96@gmail.com>
2026-05-18 21:02:30 +0530

    1-intro/exercises

--- course/1-intro/exercises/5-compiling_code/code.go
+++ course/1-intro/exercises/5-compiling_code/code.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("the-compiled Textio server is starting"
+	// fmt.Println("the-compiled Textio server is starting"
+	fmt.Println("the-compiled Textio server is starting")
 }
