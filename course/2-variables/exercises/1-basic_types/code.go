3c888ce0668b046329681fef15c11e725e3849c3
kartikeya-arun <kartikeya7.96@gmail.com>
2026-05-23 14:35:40 +0530

    complete chapter 2-variables

--- course/2-variables/exercises/1-basic_types/code.go
+++ course/2-variables/exercises/1-basic_types/code.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 	// initialize variables here
-
+	var smsSendingLimit int = 0
+	var costPerSMS float32 = 0.0
+	var hasPermission bool = false
+	var username string = ""
 	fmt.Printf("%v %f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
 }
